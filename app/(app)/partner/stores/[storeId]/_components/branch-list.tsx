852caf5349989739243f10bc736cf3bea4f0abe2
21120420 <lehuynhcuong0701@gmail.com>
2024-12-22 17:46:39 +0700

    finish branch

--- app/(app)/partner/stores/[storeId]/_components/branch-list.tsx
+++ app/(app)/partner/stores/[storeId]/_components/branch-list.tsx
@@ -2,6 +2,15 @@ import { Branch } from '../_data/schema';
 import BranchItem from './branch-item';
 
 export default function BranchList({ branchs }: { branchs: Branch[] }) {
+  if (!branchs.length) {
+    return (
+      <p className="mx-auto my-5 w-4/5 text-center text-[18px] font-semibold text-[#ececec]">
+        <span role="img">👋</span> Add your first branch by clicking on a city
+        on the map
+      </p>
+    );
+  }
+
   return (
     <ul className="flex h-[65vh] w-full list-none flex-col gap-6 overflow-x-hidden overflow-y-scroll scrollbar-hide">
       {branchs.map((branch) => (
--- app/(app)/partner/stores/[storeId]/_components/sidebar.tsx
+++ app/(app)/partner/stores/[storeId]/_components/sidebar.tsx
@@ -17,7 +17,11 @@ export default function Sidebar({
   const { isClickMap } = useMapPosition();
 
   return (
-    <div className="flex basis-1/3 flex-col items-center justify-around bg-[#2d3439] px-8 py-4">
+    <div className="flex basis-1/3 flex-col items-center bg-[#2d3439] px-8 py-4">
+      <h2 className="mb-4 pb-8 pt-5 text-center text-2xl font-semibold uppercase text-[#ececec]">
+        Branches management
+      </h2>
+
       {!isClickMap ? (
         <Suspense fallback={<Spinner />}>
           <BranchList branchs={branchs} />
